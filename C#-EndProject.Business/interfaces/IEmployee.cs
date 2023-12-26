b57a269337cfb094930e4ec2174ae1adcdda647a
Nihadname <nihadcoding@gmail.com>
2023-12-26 20:13:11 +0400

    interfaces

---
+++ C#-EndProject.Business/interfaces/IEmployee.cs
@@ -0,0 +1,21 @@
+﻿using C__EndProject.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__EndProject.Business.interfaces
+{
+    public interface IEmployee
+    {
+        Employee  Create(Employee employee);
+        Employee Update(int id,Employee employee);
+        Employee get(int id);
+        Employee delete(int id);
+        List<Employee> GetAll(int age);
+        List<Employee> GetAll(byte departmentId);
+
+
+    }
+}
